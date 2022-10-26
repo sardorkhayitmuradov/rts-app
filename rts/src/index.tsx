72935ee748e45eab9bd05ef89c04ed8c595e2ca7
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-10-26 22:34:15 +0500

    initial commit to rts-app

---
+++ rts/src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM  from "react-dom";
+
+const App = () => {
+    return <div>
+        <h1>Hello rts !</h1>
+    </div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+// export default App;
---
+++ rts/src/props/Parent.tsx
@@ -0,0 +1,6 @@
+import { Child } from './Child'
+const Parent = () => {
+  return <Child />
+}
+
+export default Parent;
