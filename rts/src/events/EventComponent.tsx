0f995338b712f3203da870d7a759e9a92ac4547f
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-10-27 01:26:53 +0500

    Drag events and other events in ts

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -2,9 +2,17 @@ const EventComponent: React.FC = () => {
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
     }
+
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log('im being dragged !!!')
+    }
+
     return (
         <div>
             <input onChange={onChange}  />
+            <div draggable onDragStart={onDragStart}>
+                Drag Me !!
+            </div>
         </div>
     )
 }
